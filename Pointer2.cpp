2206d458bdd0ac484207599fb47f207bb4b344ed
TrinhXuanBach <48061893+TrinhXuanBach@users.noreply.github.com>
2019-04-05 14:39:31 +0700

    Update Pointer2.cpp

--- Pointer2.cpp
+++ Pointer2.cpp
@@ -7,11 +7,10 @@ int main()
     int* p = new int;
     int* p2 = p;
     *p = 10;
-    delete p;
+    delete p;// lỗi ở câu lệnh này
     *p2 = 100;
 
     cout << *p2;
     delete p2;
     // giải phóng bộ nhớ p quá sớm khi con trỏ p2 đang trỏ tới vùng bộ nhớ p, vì vậy p2 đang trỏ đến vùng bộ nhớ không xác định.
-    // lỗi ở câu lệnh "cout << *p2 ";
 }
